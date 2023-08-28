0366fe9a11737ca51162c40b4ffa8cd79bddae23
devraj v <devraj.v@cisinlabs.com>
2023-08-28 11:20:46 +0530

    hello

---
+++ project-10 amitj/projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * @author Amit joshi 
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
